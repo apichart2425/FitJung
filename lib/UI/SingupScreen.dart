8f45077a2459b511c34084fcccc0a8ebd60f79a8
momoNiti <60070041@kmitl.ac.th>
2019-04-22 22:12:53 +0700

    can create user account but no info

--- lib/UI/SingupScreen.dart
+++ lib/UI/SingupScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class SingUpScreen extends StatefulWidget{
+class SingUpScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -8,18 +9,82 @@ class SingUpScreen extends StatefulWidget{
   }
 }
 
-class RegisterScreen extends State<SingUpScreen>{
+class RegisterScreen extends State<SingUpScreen> {
+  final _formkey = GlobalKey<FormState>();
+  FirebaseAuth auth = FirebaseAuth.instance;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController rePasswordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text("Sing-Up"),),
+      appBar: AppBar(
+        title: Text("Sign up"),
+      ),
       body: Padding(
-        padding: EdgeInsets.all(25),
-        child: Column(
-          children: <Widget>[
-            Text("รอจุก")
-          ],
+        padding: EdgeInsets.all(18),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: "Email"),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value.isEmpty) return "Email is required";
+                },
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(labelText: "Password"),
+                obscureText: true,
+                validator: (value) {
+                  if (value.isEmpty && value.length < 8)
+                    return "Password is required";
+                },
+              ),
+              TextFormField(
+                controller: rePasswordController,
+                decoration: InputDecoration(labelText: "Re-Password"),
+                obscureText: true,
+                validator: (value) {
+                  if (value.isEmpty && value.length < 8)
+                    return "Re-Password is required";
+                },
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: RaisedButton(
+                      child: Text("Next"),
+                      onPressed: () {
+                        if (_formkey.currentState.validate()) {
+                          auth
+                              .createUserWithEmailAndPassword(
+                                  email: emailController.text,
+                                  password: passwordController.text)
+                              // .then((FirebaseUser user) {
+                            // user.sendEmailVerification().then((user) {
+                              // Navigator.pop(context);
+                            // });
+                            // print("return from firebase  $user.email");
+                          // })
+                          .catchError((error) {
+                            print("$error");
+                          });
+                        } else {
+                          print("error");
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
