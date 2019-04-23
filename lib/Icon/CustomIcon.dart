e55719cb6f93b9c0801a50d33392ff90d2f969f1
khingbmc <ratanon.jan@gmail.com>
2019-04-23 21:31:37 +0700

    add ui home

---
+++ lib/Icon/CustomIcon.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomIcon{
+  static const IconData menu = IconData(0xe900, fontFamily: "CustomIcon");
+  static const IconData option = IconData(0xe902, fontFamily: "CustomIcon");
+}
---
+++ lib/data.dart
@@ -0,0 +1,11 @@
+List<String> images = [
+  "resource/image_01.jpg"
+  "resource/image_02.png",
+  "resource/image_03.jpg",
+];
+
+List<String> title = [
+  "Upper Body",
+  "Full Body",
+  "Lower Body",
+];
