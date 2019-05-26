455c879735a8e6c6638ca4c4e23e608ba5fa7145
khingbmc <ratanon.jan@gmail.com>
2019-05-27 06:25:59 +0700

    add splash

--- lib/UI/ModeScreen.dart
+++ lib/UI/ModeScreen.dart
@@ -48,103 +48,118 @@ class ModeScreen extends StatelessWidget{
               Padding(
                 padding: EdgeInsets.only(bottom: 90),
               ),
-              SizedBox(
-                height: 100.0,
-                child: Padding(
-                  padding: EdgeInsets.only(right: 20.0, left: 20.0),
-                  child: Card(
-                  color: Color(0xFF121e2f) ,
-                  
-                  child: Center(
-                    child: Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(right: 20.0),
-                        ),
-                        Image.asset('resource/easy.png', width: 42, height: 42,),
-                        Padding(
-                          padding: EdgeInsets.only(right: 30),
-                        ),
-                        Text('EASY', style:TextStyle(
-                          color: Colors.white,
-                          fontSize: 42
-                        )),
-                        Padding(
-                          padding: EdgeInsets.only(left: 70),
-                          child: Icon(Icons.arrow_forward_ios, size: 30, color: Colors.white,),
-                        ),
-                        
-                      ],
-                    )
-                  )
-              ),
-                ),
-              ),
-              SizedBox(
-                height: 100.0,
-                child: Padding(
-                  padding: EdgeInsets.only(right: 20.0, left: 20.0),
-                  child: Card(
-                  color: Color(0xFF121e2f) ,
-                  
-                  child: Center(
-                    child: Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(right: 20.0),
-                        ),
-                        Image.asset('resource/medium.png', width: 42, height: 42,),
-                        Padding(
-                          padding: EdgeInsets.only(right: 30),
-                        ),
-                        Text('MEDIUM', style:TextStyle(
-                          color: Colors.white,
-                          fontSize: 42
-                        )),
-                        Padding(
-                          padding: EdgeInsets.only(left: 10.0),
-                          child: Icon(Icons.arrow_forward_ios, size: 30, color: Colors.white,),
-                        ),
-
-                      ],
-                    )
-                  )
-              ),
-                ),
-              ),
-              SizedBox(
-                height: 100.0,
-                child: Padding(
-                  padding: EdgeInsets.only(right: 20.0, left: 20.0),
-                  child: Card(
+              InkWell(
+                  onTap: (){
                     
-                  color: Color(0xFF121e2f) ,
-      
+                  },
+                  child: SizedBox(
+                  height: 100.0,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 20.0, left: 20.0),
+                    child: Card(
+                    color: Color(0xFF121e2f) ,
+                    
                     child: Center(
                       child: Row(
                         children: <Widget>[
                           Padding(
                             padding: EdgeInsets.only(right: 20.0),
                           ),
-                          Image.asset('resource/hard.png', width: 42, height: 42,),
+                          Image.asset('resource/easy.png', width: 42, height: 42,),
                           Padding(
                             padding: EdgeInsets.only(right: 30),
                           ),
-                          Text('HARD', style:TextStyle(
+                          Text('EASY', style:TextStyle(
                             color: Colors.white,
                             fontSize: 42
                           )),
                           Padding(
-                          padding: EdgeInsets.only(left: 60.0),
-                          child: Icon(Icons.arrow_forward_ios, size: 30, color: Colors.white,),
-                        ),
-                        
+                            padding: EdgeInsets.only(left: 70),
+                            child: Icon(Icons.arrow_forward_ios, size: 30, color: Colors.white,),
+                          ),
+                          
+                        ],
+                      )
+                    )
+                ),
+                  ),
+                ),
+              ),
+              InkWell(
+                  onTap: (){
+
+                  },
+                  child: SizedBox(
+                  height: 100.0,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 20.0, left: 20.0),
+                    child: Card(
+                    color: Color(0xFF121e2f) ,
+                    
+                    child: Center(
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(right: 20.0),
+                          ),
+                          Image.asset('resource/medium.png', width: 42, height: 42,),
+                          Padding(
+                            padding: EdgeInsets.only(right: 30),
+                          ),
+                          Text('MEDIUM', style:TextStyle(
+                            color: Colors.white,
+                            fontSize: 42
+                          )),
+                          Padding(
+                            padding: EdgeInsets.only(left: 10.0),
+                            child: Icon(Icons.arrow_forward_ios, size: 30, color: Colors.white,),
+                          ),
 
                         ],
                       )
-                    ),
-                  
+                    )
+                ),
+                  ),
+                ),
               ),
+              InkWell(
+                onTap: (){
+                  
+                },
+                child: SizedBox(
+                              height: 100.0,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 20.0, left: 20.0),
+                    child: Card(
+                      
+                    color: Color(0xFF121e2f) ,
+      
+                      child: Center(
+                        child: Row(
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(right: 20.0),
+                            ),
+                            Image.asset('resource/hard.png', width: 42, height: 42,),
+                            Padding(
+                              padding: EdgeInsets.only(right: 30),
+                            ),
+                            Text('HARD', style:TextStyle(
+                              color: Colors.white,
+                              fontSize: 42
+                            )),
+                            Padding(
+                            padding: EdgeInsets.only(left: 60.0),
+                            child: Icon(Icons.arrow_forward_ios, size: 30, color: Colors.white,),
+                          ),
+                          
+
+                          ],
+                        )
+                      ),
+                    
+                ),
+                  ),
                 ),
               )
             ],
