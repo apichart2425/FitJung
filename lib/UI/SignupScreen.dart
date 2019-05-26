e076f309dd0dbc180cfb01de8ebd51857d676ad7
khingbmc <ratanon.jan@gmail.com>
2019-05-26 21:44:03 +0700

    add logo

--- lib/UI/SignupScreen.dart
+++ lib/UI/SignupScreen.dart
@@ -1,6 +1,7 @@
 import 'package:fitjung/UI/SignInfoScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SignUpScreen extends StatefulWidget {
   @override
@@ -18,19 +19,88 @@ class SignUpScreenState extends State<SignUpScreen> {
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334, allowFontScaling: true);
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sign up"),
-      ),
-      body: SingleChildScrollView(
-              child: Padding(
-          padding: EdgeInsets.all(18),
-          child: Form(
-            key: _formkey,
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomPadding: false,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 20.0),
+            child: Image.asset('resource/6272.jpg'),
+          ),
+          Expanded(
+            child: Container(
+
+            ),
+            
+          ),
+          Image.asset('resource/img_02.png'),
+          ],
+        ),
+        SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(left: 28.0, right: 28.0, top: 60.0),
             child: Column(
               children: <Widget>[
-                TextFormField(
+                Row(
+                  children: <Widget>[
+                    Image.asset('resource/logo.png',
+                      width: ScreenUtil.getInstance().setWidth(110),
+                      height: ScreenUtil.getInstance().setHeight(110),
+                    ),
+                    Text("FITJUNG", style: TextStyle(
+                      fontFamily: 'Poppins-Bold',
+                      fontSize: ScreenUtil.getInstance().setSp(46),
+                      letterSpacing: .6,
+                      fontWeight: FontWeight.bold,
+                    ),)
+                  ],
+                ),
+                SizedBox(
+                  height: ScreenUtil.getInstance().setHeight(180),
+                ),
+                Form(
+                  key: _formkey,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                  width: double.infinity,
+                  height: ScreenUtil.getInstance().setHeight(500),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(0.0, -10.0),
+                        blurRadius: 15.0
+                      ),
+                      BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(0.0, -10.0),
+                        blurRadius: 10.0
+                      ),
+                      BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(0.0, 10.0),
+                        blurRadius: 10.0
+                      ),
+                    ]
+                    ),
+                    child: Padding(
+                      padding: 
+                    EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        TextFormField(
                   controller: emailController,
                   decoration: InputDecoration(labelText: "Email"),
                   keyboardType: TextInputType.emailAddress,
@@ -77,7 +147,12 @@ class SignUpScreenState extends State<SignUpScreen> {
                     }
                   },
                 ),
-                Row(
+                      ],
+                      
+                    ),
+                    ),
+                  ),
+                    Row(
                   children: <Widget>[
                     Expanded(
                       flex: 1,
@@ -97,12 +172,27 @@ class SignUpScreenState extends State<SignUpScreen> {
                       ),
                     ),
                   ],
-                ),
+                ),],
+                  ),)
               ],
             ),
           ),
         ),
+        ],
       ),
     );
   }
 }
+// SingleChildScrollView(
+//               child: Padding(
+//           padding: EdgeInsets.all(18),
+//           child: Form(
+//             key: _formkey,
+//             child: Column(
+//               children: <Widget>[
+                
+//               ],
+//             ),
+//           ),
+//         ),
+//       )
